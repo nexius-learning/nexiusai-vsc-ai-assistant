e593b882b605c3a3e2236e4c8b438f6c53da0286
Kovacs Zsolt <kovacs.zsolt@nexius.hu>
2025-01-04 23:50:59 +0100

    Rename and streamline AI assistant; remove unused features
    Renamed the AI assistant to "Nexius AI Code Bug Hunter" and updated relevant metadata. Removed deprecated commands and settings, including unit test generation functionality and LangSmith integration, to simplify the extension. Focused on optimizing the code review feature for better clarity and usability.

--- extension.js
+++ extension.js
@@ -1,11 +1,8 @@
-const registerDisposableNexiusAIAssistantGenerateUnitTest = require('./command.nexiusai-assistant-generate-unit-test');
 const registerDisposableNexiusAIAssistantCodeCheck = require('./command.nexiusai-assistant-code-check');
-const registerDisposableNexiusAIAssistantTest = require("./command.nexiusai-assistant-test");
+
 
 function activate(context) {
-    registerDisposableNexiusAIAssistantGenerateUnitTest(context);
     registerDisposableNexiusAIAssistantCodeCheck(context);
-    registerDisposableNexiusAIAssistantTest(context);
 }
 
 function deactivate() {
